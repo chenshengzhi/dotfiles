5cd4ff72f6552d401c6ce434e6753ca0f71a7f4b
chenshengzhi <329012084@qq.com>
2016-09-24 18:40:44 +0800

    2016年 9月24日 星期六 18时35分26秒 CST

--- backup.sh
+++ backup.sh
@@ -13,9 +13,9 @@ function checkAndCopy()
 checkAndCopy "~/.zshrc"
 checkAndCopy "~/.lldbinit"
 checkAndCopy "~/.vimrc"
-checkAndCopy "~/.vimrc.bundles" 
-checkAndCopy "~/.gvimrc" 
-checkAndCopy "~/.ycm_extra_conf.py" 
+checkAndCopy "~/.vimrc.bundles"
+checkAndCopy "~/.gvimrc"
+checkAndCopy "~/.ycm_extra_conf.py"
 
 if [ -d ~/.vim/MySnippets ];then
     rm -rf ./init/vim/MySnippets
@@ -31,11 +31,13 @@ if [ `uname` == 'Darwin' ];then
         mkdir -p ./init/sublime
         cp -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ./init/sublime/
     fi
-    
+
     rm -rf ./init/Xcode
     mkdir -p ./init/Xcode
-    cp -R ~/Library/Developer/Xcode/UserData ./init/Xcode/UserData
-elif [ `uname` == 'Linux' ];then 
+    cp -R ~/Library/Developer/Xcode/UserData/CodeSnippets ./init/Xcode/UserData/CodeSnippets
+    cp -R ~/Library/Developer/Xcode/UserData/FontAndColorThemes ./init/Xcode/UserData/FontAndColorThemes
+    cp -R ~/Library/Developer/Xcode/UserData/KeyBindings ./init/Xcode/UserData/KeyBindings
+elif [ `uname` == 'Linux' ];then
    if [ -d .config/sublime-text-3/Packages/User ];then
         rm -rf ./init/sublime
         mkdir -p ./init/sublime
