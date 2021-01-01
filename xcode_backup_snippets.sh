c598e7a730e1ded1106bf28968d23bb71e42b4ef
chenshengzhi <329012084@qq.com>
2021-01-01 09:45:37 +0800

    Fri Jan  1 09:45:37 CST 2021

---
+++ xcode_backup_snippets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ./init/Xcode/UserData/CodeSnippets
+cp -rf ~/Library/Developer/Xcode/UserData/CodeSnippets ./init/Xcode/UserData/CodeSnippets
---
+++ xcode_recovery_snippets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp -rf ./init/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
